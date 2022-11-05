d06229c7e8c21f7f355e3f294e7d8244d2ae0864
unknown <akshayhegde158@gmail.com>
2022-11-05 13:15:48 +0530

    form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "./Form.css"
+import axios from 'axios'
 
 const Form = () => {
+
+    const [data,setData] = useState({
+        botName:'',
+        workspaceName:"",
+        channelName:"",
+        slackToken:"",
+    })
+
+    const handleSubmit = async ()=>{
+        const data = await axios.post("url",data)
+    }
+
     return (
         <div className='formBody'>
             <div className="container">
                 <div className="brand-title">Bot Maker</div>
                 <div className="inputs">
                   
-                    <input className="inpt" type="text" placeholder="enter bot name" />
+                    <input onChange={()=>{setData({...data,botName:e.target.value})}} value={data.botName} className="inpt" type="text" placeholder="enter bot name" />
                    
-                    <input className="inpt" type="text" placeholder="enter workspace name" />
+                    <input onChange={()=>{setData({...data,workspaceName:e.target.value})}} value={data.workspaceName} className="inpt" type="text" placeholder="enter workspace name" />
                    
-                    <input className="inpt" type="text" placeholder="enter channle name" />
+                    <input onChange={()=>{setData({...data,channelName:e.target.value})}} value={data.channelName} className="inpt" type="text" placeholder="enter channle name" />
                   
-                    <input className="inpt" type="text" placeholder="enter slack token" />
-                    <button className="btn" type="submit">Submit</button>
+                    <input onChange={()=>{setData({...data,slackToken:e.target.value})}} value={data.slackToken} className="inpt" type="text" placeholder="enter slack token" />
+                    <button className="btn" onClick={handleSubmit} type="button">Submit</button>
                 </div>
             </div>
         </div>

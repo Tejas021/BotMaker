5c108ddf43c1fee0808e71472e30147c92a0b0b0
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2022-11-07 00:01:36 +0530

    server

--- server/routes/botRoutes.js
+++ server/routes/botRoutes.js
@@ -1,7 +1,43 @@
 const express = require('express')
 const router = express.Router()
 router.post("/",async(req,res)=>{
-    console.log(req.body);
+    fetch("https://slack.com/api/apps.manifest.create",{
+        method:"POST",
+        headers:{
+            Authorization:`Bearer ${req.body.token}`
+        },
+        body:JSON.stringify({
+            "manifest":{
+         "display_information": {
+                "name": req.body.botName
+            },
+            "settings": {
+                "org_deploy_enabled": false,
+                "socket_mode_enabled": false,
+                "is_hosted": false,
+                "token_rotation_enabled": false
+            },
+            "features":{
+                "bot_user":{
+                    "display_name":"Tejas"
+                }
+            },
+           "oauth_config":{
+         "scopes":{
+           "bot":[
+            "commands",
+              "chat:write",
+              "chat:write.public"
+           ]
+            
+         }
+         
+           }
+ 
+            }
+           
+        })
+    })
     res.send(req.body)
 })
 
